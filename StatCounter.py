5e9256bbd0140bf7ff5eb90829b9c8b322f697fd
Kirk Scheibelhut <kjs@google.com>
2019-05-16 17:29:23 -0700

    Add sort order

--- StatCounter.py
+++ StatCounter.py
@@ -252,9 +252,9 @@ encounterfile.close()
 
 #sort by weighted usage
 if tier in ['challengecup1v1','1v1']:
-	pokes=sorted(pokes, key=lambda pokes:-pokes[2])
+	pokes=sorted(pokes, key=lambda pokes:(-pokes[2], pokes[0]))
 else:
-	pokes=sorted(pokes, key=lambda pokes:-pokes[3])
+	pokes=sorted(pokes, key=lambda pokes:(-pokes[3], pokes[0]))
 p=[]
 usagefile.write(" Total battles: "+str(battleCount)+"\n")
 try:
@@ -289,7 +289,7 @@ if t not in nonSinglesFormats and t not in ['1v1','challengecup1vs1']: #lead sta
 	pokes = []
 	for i in pokedict:
 		pokes.append([i]+pokedict[i])
-	pokes=sorted(pokes, key=lambda pokes:-pokes[2])
+	pokes=sorted(pokes, key=lambda pokes:(-pokes[2], pokes[0]))
 	leadsfile.write(" Total leads: "+str(battleCount*2)+"\n")
 	leadsfile.write(" + ---- + ------------------ + --------- + ------ + ------- + \n")
 	leadsfile.write(" | Rank | Pokemon            | Usage %   | Raw    | %       | \n")
@@ -306,7 +306,7 @@ if metagamefile:
 	tags = []
 	for tag in tagCounter:
 		tags.append([tag,tagCounter[tag]])
-	tags=sorted(tags, key=lambda tags:-tags[1])
+	tags=sorted(tags, key=lambda tags:(-tags[1], tags[0]))
 
 	for i in range(0,len(tags)):
 		line = ' '+tags[i][0]
